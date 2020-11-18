d0d70ef7228c8a3627fcdb524d734aebe8d827c2
Jacob <Jacob.SH@icloud.com>
2020-11-18 13:45:08 +0100

    Added MyMethod To Main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main{
     public static void main(String[] args) {
         System.out.println("yo");
+        MyMethod object1 = new MyMethod();
+        object1.myMethod();
     }
 }
